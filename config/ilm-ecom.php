ec9984c2b2f1d21de84b994ef04b7b00759f3d9e
mjk-limon <jhmasterlimon11@gmail.com>
2025-02-04 18:42:41 +0600

    Core: Config updated and Cleanup

--- config/ilm-ecom.php
+++ config/ilm-ecom.php
@@ -5,7 +5,7 @@ return [
     'client-id' => env('ILM_CLIENT_ID', ''),
     'client-secret' => env('ILM_CLIENT_SECRET', ''),
 
-    'sandbox' => env('ILM_IS_SANDBOX', true),
+    'sandbox' => (bool) env('ILM_IS_SANDBOX', true),
 
     'url' => 'https://sales-erp.live.jahidlimon.com',
     'sandbox-url' => 'https://sales-erp.server.jahidlimon.com',
