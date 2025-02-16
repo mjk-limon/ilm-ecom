3b965ca5bd1c08cfb4b31647426521f4955d1d21
mjk-limon <jhmasterlimon11@gmail.com>
2025-02-16 14:50:01 +0600

    Set test configuration and cache values in the environment setup

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -1,14 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\{Cache, Config};
 use Ilm\Ecom\{Air, Services\ResourceController};
 
 it('can test application', function () {
-    Config::set('ilm-ecom.sandbox-url', 'https://creative.jahidlimon.com/ilm-ecom');
-    Config::set('ilm-ecom.client-id', '9e255dee-2bbb-4e29-aed4-70e365426c2a');
-    Config::set('ilm-ecom.client-secret', 'jr0aDtt9JW4ZxeYJqMTs1R134NxjIvuL4VHm7fsG');
-    Cache::set('87d3cf2fcda098612f21cfc9a4756d4f', ['success' => true]);
-
     // Air::$defaultResponseProvider = function ($r, $rc) {
     //     expect($r)->toBe('errors/403');
     //     return $rc;
@@ -32,7 +26,7 @@ it('can test application', function () {
 
 
     $response = $controller->index();
-    $new_cached = Cache::get('87d3cf2fcda098612f21cfc9a4756d4f');
+    $new_cached = cache('87d3cf2fcda098612f21cfc9a4756d4f');
 
     expect($response)->toBeArray();
     expect($new_cached)->toBeArray();
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -27,6 +27,11 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
+        config()->set('ilm-ecom.sandbox-url', 'https://creative.jahidlimon.com/ilm-ecom');
+        config()->set('ilm-ecom.client-id', '9e255dee-2bbb-4e29-aed4-70e365426c2a');
+        config()->set('ilm-ecom.client-secret', 'jr0aDtt9JW4ZxeYJqMTs1R134NxjIvuL4VHm7fsG');
+
+        cache()->set('87d3cf2fcda098612f21cfc9a4756d4f', ['success' => true]);
 
         // foreach (\Illuminate\Support\Facades\File::allFiles(__DIR__ . '/../database/migrations') as $migration) {
         //     (include $migration->getRealPath())->up();
